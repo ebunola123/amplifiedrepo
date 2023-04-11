2c03e909a9642a66e31678bf24be3e374cff0d1d
Ebunola Ajayi <c19324126@mytudublin.ie>
2023-04-11 20:58:49 +0100

    10th Commit - checkout class still in progress

--- app/src/main/java/com/example/amplifiedelectricals/CustomerCheckoutActivity.java
+++ app/src/main/java/com/example/amplifiedelectricals/CustomerCheckoutActivity.java
@@ -1,17 +1,31 @@
 package com.example.amplifiedelectricals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class CustomerCheckoutActivity extends AppCompatActivity {
 
     //ArrayList<ModelCart> cartList;
+    EditText cardNameET, cardNoET, expiryDateET, cvcET, addressLine1ET, addressLine2ET, countyET, countryET;
+    CustomerCartListAdapter adapter;
+    RecyclerView recyclerView;
+    ArrayList<ModelCart> cartList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +49,87 @@ public class CustomerCheckoutActivity extends AppCompatActivity {
         //if exists, do stockItemQuantity - orderQuantity
 
 
+
+
+       // DatabaseReference reference =
+
+        FirebaseAuth auth= FirebaseAuth.getInstance();
+        FirebaseUser currentUser=auth.getCurrentUser();
+
+        if(currentUser==null){
+            Intent intent=new Intent(this, CustomerLoginActivity.class);
+            startActivity(intent);
+        }
+
+        String customerID = auth.getUid();
+
         Intent i = getIntent();
         String total = i.getStringExtra("totalPrice");
         System.out.println("MY TOTAL - " + total);
 
-       // DatabaseReference reference =
 
+        //payment details
+        cardNameET = findViewById(R.id.cardNameET);
+        cardNoET = findViewById(R.id.cardNoET);
+        expiryDateET = findViewById(R.id.expiryDateET);
+        cvcET = findViewById(R.id.cvcET);
+
+        //deleivery address details
+        addressLine1ET = findViewById(R.id.addressLine1ET);
+        addressLine2ET = findViewById(R.id.addressLine2ET);
+        countyET = findViewById(R.id.countyET);
+        countryET = findViewById(R.id.countryET);
+
+
+
+        //recycler view details
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cartList = new ArrayList<>();
+        adapter = new CustomerCartListAdapter(cartList, this);
+        recyclerView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Cart").child(customerID);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    ModelCart cart = dataSnapshot.getValue(ModelCart.class);
+                    cartList.add(cart);
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+
+
+        });
+
+        DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference("Customers").child(customerID);
+        reference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ModelCustomers customer = snapshot.getValue(ModelCustomers.class);
+
+                addressLine1ET.setText(customer.getAddressLine1());
+                addressLine2ET.setText(customer.getAddressLine2());
+                countyET.setText(customer.getCounty());
+                countryET.setText(customer.getCountry());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        //open stock db
+        //if cartlist.contains any of the values
+        
+        
     }
 }
